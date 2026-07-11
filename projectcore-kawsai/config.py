dae52e507c12374a0297106aea9a4a31efccd43a
Seroluce <141604179+Seroluce@users.noreply.github.com>
2026-07-11 14:26:55 -0500

    refactor(kawsai): añadir soporte para Qdrant y optimizar procesamiento de estudiantes y ofertas
    - Se añadió configuración para Qdrant en `config.py` y `docker-compose.yml`, incluyendo un nuevo servicio `kawsai`.
    - Se creó un `Dockerfile` para el servicio `kawsai`, con instalación de dependencias necesarias para procesamiento.
    - Se optimizaron métodos en servicios y rutas de estudiantes y ofertas para mejorar serialización y manejo de datos enriquecidos.
    - Se implementó lógica interna para serialización de datos en `FilterMatchPortImpl`, reduciendo duplicación y mejorando manejo de fechas y listas.

--- projectcore-kawsai/config.py
+++ projectcore-kawsai/config.py
@@ -1,7 +1,12 @@
 import os
+
 from dotenv import load_dotenv
 
-#solo para usarlo local
 load_dotenv()
+
+QDRANT_HOST = os.getenv(
+    "QDRANT_HOST",
+    "http://localhost:6333",
+)
 
 BACKEND_API_URL = os.getenv("BACKEND_API_URL")
